f101a2e7550fd03927a31d73070613e9f5b12bb7
Follmer <follmerka@gmail.com>
2019-03-04 11:50:08 -0700

    wip

--- main.go
+++ main.go
@@ -5,15 +5,18 @@ import (
     "log"
     "net/http"
     "github.com/gorilla/mux"
+    "strconv"
+    // "fmt"
+    // "net/http/httputil"
 )
 
 type Book struct {
-	ID 			string	`json:id,omitempty`
+	ID 			int		`json:id,omitempty`
 	Title		string	`json:title,omitempty`
 	Author		string	`json:author,omitempty`
 	Publisher	string	`json:publisher,omitempty`
 	PublishDate	string	`json:publishdate,omitempty`
-	Rating		int		`json:rating,omitempty`
+	Rating		int64		`json:rating,omitempty`
 	Status		bool	`json:status,omitempty`
 }
 
@@ -22,15 +25,15 @@ var books []Book
 // Handle endpoints
 func main() {
 
-	books = append(books, Book{ID: "1", Title: "Harry Potter and the Sorcerer's Stone", Author: "J.K. Rowling", Publisher: "Bloomsbury", PublishDate: "06/26/1997", Rating: 3, Status: false})
-	books = append(books, Book{ID: "2", Title: "The Lord of the Rings", Author: "J.R.R. Tolkien", Publisher: "Allen & Unwin", PublishDate: "07/29/1954", Rating: 2, Status: false})
-	books = append(books, Book{ID: "3", Title: "The Cat in the Hat", Author: "Dr. Seuss", Publisher: "Random House", PublishDate: "03/12/1957", Rating: 1, Status: true})
+	books = append(books, Book{ID: 1, Title: "Harry Potter and the Sorcerer's Stone", Author: "J.K. Rowling", Publisher: "Bloomsbury", PublishDate: "06/26/1997", Rating: 3, Status: false})
+	books = append(books, Book{ID: 2, Title: "The Lord of the Rings", Author: "J.R.R. Tolkien", Publisher: "Allen & Unwin", PublishDate: "07/29/1954", Rating: 2, Status: false})
+	books = append(books, Book{ID: 3, Title: "The Cat in the Hat", Author: "Dr. Seuss", Publisher: "Random House", PublishDate: "03/12/1957", Rating: 1, Status: true})
 
 	router := mux.NewRouter()
 
 	router.HandleFunc("/books", GetBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", GetBook).Methods("GET")
-	router.HandleFunc("/books/{id}", CreateBook).Methods("POST")
+	router.HandleFunc("/books", CreateBook).Methods("POST")
 	router.HandleFunc("/books/{id}", DeleteBook).Methods("DELETE")
 
 	log.Fatal(http.ListenAndServe(":8000", router))
@@ -45,7 +48,8 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 func GetBook(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	for _, book := range books {
-		if book.ID == params["id"] {
+		print(params["id"])
+		if book.ID == 3 {
 			json.NewEncoder(w).Encode(book)
 		}
 	}
@@ -53,19 +57,54 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 
 // Creates a book with id
 func CreateBook(w http.ResponseWriter, r *http.Request) {
+	
+
+	if r.Body == nil {
+		http.Error(w, "Please send in a request body", 400)
+		return
+	}
+
 	params := mux.Vars(r)
 	var book Book
 	_ = json.NewDecoder(r.Body).Decode(&book)
-	book.ID = params["id"]
-	books = append(books, book)
-	json.NewEncoder(w).Encode(books)
+
+	// dump, _ := httputil.DumpRequest(r, true)
+
+
+	print(r.FormValue("title"))
+
+	if 	(book.ID != 0 && 
+		book.Title != "" && 
+		book.Author != "" &&
+		book.Publisher != "" &&
+		book.PublishDate != "" &&
+		book.Rating != 0) {
+		// book.Status != nil) {
+
+		// book.ID, _ = params["id"]
+		print(params["id"])
+		book.Title = params["title"]
+		book.Author = params["author"]
+		book.Publisher = params["publisher"]
+		book.PublishDate = params["publishdate"]
+		book.Rating, _ = strconv.ParseInt(params["rating"], 0, 64)
+		book.Status, _ = strconv.ParseBool(params["status"])	
+
+		books = append(books, book)
+		json.NewEncoder(w).Encode(books)
+
+	} else {
+		log.Fatal("Missing body param")
+	}
+	
+	
 }
 
 // Deletes a book, by id
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
-	params := mux.Vars(r)
+	// params := mux.Vars(r)
 	for index, book := range books {
-		if book.ID == params["id"] {
+		if book.ID == 3 {
 			books = append(books[:index], books[:index+1]...)
             break
 		}
