fb25ac417783dfaeb46ac6a9829b2b3e02b90443
Kyle Follmer <follmerka@gmail.com>
2019-03-06 10:43:39 -0700

    wip

---
+++ sql/insertData.sql
@@ -0,0 +1,8 @@
+INSERT INTO books (title, author, publisher, publishdate, rating, status)
+VALUES("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Bloomsbury", "06/26/1997", 3, 0)
+
+-- INSERT INTO books(title, author, publisher, publishdate, rating, status)
+-- VALUES("The Lord of the Rings", "J.R.R. Tolkien", "Allen & Unwin", "07/29/1954", 1, 1)
+--
+-- INSERT INTO books(title, author, publisher, publishdate, rating, status)
+-- VALUES("The Cat in the Hat", "Dr. Seuss", "Random House", "03/12/1957", 2, 0)
