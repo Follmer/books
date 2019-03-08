10bbfa39d03f7e97e1ce7c51bb819afc77ed5606
Kyle Follmer <follmerka@gmail.com>
2019-03-08 10:45:00 -0700

    broken state

--- sql/createTable.sql
+++ sql/createTable.sql
@@ -10,3 +10,12 @@ CREATE TABLE IF NOT EXISTS books
     rating INT DEFAULT 0,
     status INT DEFAULT 0
 );
+
+INSERT INTO books (title, author, publisher, publishdate, rating, status)
+VALUES("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", "Bloomsbury", "06/26/1997", 3, false)
+
+INSERT INTO books (title, author, publisher, publishdate, rating, status)
+VALUES("The Lord of the Rings", "J.R.R. Tolkien", "Allen & Unwin", "07/29/1954", 1, true)
+
+INSERT INTO books (title, author, publisher, publishdate, rating, status)
+VALUES("The Cat in the Hat", "Dr. Seuss", "Random House", "03/12/1957", 2, false)
